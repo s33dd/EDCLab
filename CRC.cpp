d07990d0bfc5025cf982ab88cfdf8e6f9074787b
s33dd <dsuhoverikov@gmail.com>
2022-11-15 20:03:37 +0200

    CRC-24 realization

---
+++ CRC.cpp
@@ -0,0 +1,92 @@
+#include "CRC.h"
+
+CRC::CRC() {
+	polynomial = 0x864CFB;
+	buffer = 0;
+}
+
+void CRC::Chosen() {
+	int choice;
+	while (true) {
+		std::cout << std::endl << "Choose what are you want to do:" << std::endl <<
+			"1 - Calculate checksum" << std::endl << "2 - Check file" << std::endl;
+		std::cin >> choice;
+		switch (choice) {
+		case 1: {
+			this->buffer = 0;
+			Reader rdr = Reader(Reader::InputPath());
+			while (!rdr.IsEnd()) {
+				rdr.Read(PACKAGE);
+				Calc(rdr.GetInfo());
+			}
+			ZerosJoining();
+			break;
+		}
+
+		case 2: {
+			if (this->buffer == 0) {
+				std::cout << "You must calc checksum first!" << std::endl;
+				break;
+			}
+			CRC inspected = CRC();
+			Reader rdr = Reader(Reader::InputPath());
+			while (!rdr.IsEnd()) {
+				rdr.Read(PACKAGE);
+				inspected.Calc(rdr.GetInfo());
+			}
+			inspected.ZerosJoining();
+			Check(inspected.GetChecksum());
+			break;
+		}
+		default:
+			exit(EXIT_SUCCESS);
+		}
+	}
+}
+
+void CRC::Calc(std::vector<std::byte> info) {
+	std::byte mlt{ 128 }; // byte {1000 0000} for taking the leftmost bit
+	for (auto byte : info) {
+		std::byte tmp{ 0 };
+		for (int i = 0; i < BYTE_SIZE; i++) {
+			tmp = byte & mlt;
+			tmp >>= BYTE_SIZE - 1;
+			buffer <<= 1;
+			buffer |= std::bitset<POLY_SIZE>(std::to_integer<int>(tmp));
+			if (buffer[POLY_SIZE - 1] == 1) {
+				buffer ^= polynomial;
+			}
+			byte <<= 1;
+		}
+	}
+}
+
+void CRC::ZerosJoining() {
+	std::bitset<1> zeroBit{ 0 };
+	for (int i = 0; i < POLY_SIZE - 1; i++) {
+		buffer <<= 1;
+		buffer[0] = zeroBit[0];
+		if (buffer[POLY_SIZE - 1] == 1) {
+			std::cout << buffer << std::endl;
+			buffer ^= polynomial;
+			std::cout << buffer << std::endl;
+		}
+	}
+}
+
+void CRC::Check(std::bitset<POLY_SIZE - 1> sum) {
+	if (this->GetChecksum() == sum) {
+		std::cout << std::endl << "Verification passed" << std::endl;
+	}
+	else {
+		std::cout << std::endl << "Verification is not passed" << std::endl;
+	}
+}
+
+std::bitset<POLY_SIZE - 1> CRC::GetChecksum() {
+	std::bitset<POLY_SIZE - 1> checkSum;
+	for (int i = POLY_SIZE - 2; i >= 0; i--) {
+		checkSum[i] = buffer[i];
+	}
+	return checkSum;
+}
---
+++ CRC.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <bitset>
+#include "Reader.h"
+#define POLY_SIZE 24
+#define BYTE_SIZE 8
+#define PACKAGE 3
+
+class CRC {
+private:
+	std::bitset<POLY_SIZE> polynomial;
+	std::bitset<POLY_SIZE> buffer;
+public:
+	CRC();
+	void Chosen();
+	void Calc(std::vector<std::byte> info);
+	void ZerosJoining();
+	void Check(std::bitset<POLY_SIZE - 1> sum);
+	std::bitset<POLY_SIZE - 1> GetChecksum();
+};
+
